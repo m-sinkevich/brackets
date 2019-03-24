cd7ffb853ad5cfbb4af56a75aed2091af7022130
Karlson <peterpan@tut.by>
2019-03-24 18:39:34 +0300

    feat: create solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let i = 0; i < str.length; i++) {
+        for (let k = 0; k < bracketsConfig.length; k++) {
+            if (str[i] == bracketsConfig[k][0] && str[i+1] == bracketsConfig[k][1]) {
+                str = str.slice(0, i) + str.slice(i + 2);
+                i = i - 2;
+            }
+        }
+    }
+    return  !str.length ;
 }
